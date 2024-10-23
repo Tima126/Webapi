718310e17cfa03a485e060fd3275f89b9beec07d
Tim <148056216+Tima126@users.noreply.github.com>
2024-10-23 14:04:38 +0300

    Revert "Client"
    This reverts commit 9a4bb92ccab9b276c3bfc53b20c4297ffe0b99e3.

--- webApi/obj/Debug/net8.0/webApi.MvcApplicationPartsAssemblyInfo.cs
+++ webApi/obj/Debug/net8.0/webApi.MvcApplicationPartsAssemblyInfo.cs
@@ -11,13 +11,7 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("BlazorApp1")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
-[assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dde9e58740a5af51d8e3ab88ef67dfad31f54699")]
-[assembly: System.Reflection.AssemblyProductAttribute("BlazorApp1")]
-[assembly: System.Reflection.AssemblyTitleAttribute("BlazorApp1")]
-[assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
+[assembly: Microsoft.AspNetCore.Mvc.ApplicationParts.ApplicationPartAttribute("Swashbuckle.AspNetCore.SwaggerGen")]
 
 // Создано классом WriteCodeFragment MSBuild.
 
