2deec9928f54d4f4983b22fe73d0e8a17b5e1def
Tim <148056216+Tima126@users.noreply.github.com>
2024-10-23 14:04:19 +0300

    Revert "Client"
    This reverts commit 9a4bb92ccab9b276c3bfc53b20c4297ffe0b99e3.

--- webApi/obj/Debug/net8.0/webApi.AssemblyInfo.cs
+++ webApi/obj/Debug/net8.0/webApi.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("webApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+80a2e3375a712e771701b1dfd08d3cccf5fd1d63")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+90c241a16d02b74105b92254a49c5647e4ece7ab")]
 [assembly: System.Reflection.AssemblyProductAttribute("webApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("webApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
